512622a3fc0222dd55b834be407882481ba3c617
Peeyush Gupta <peeyushgupta279@gmail.com>
2025-07-29 15:50:21 +0530

    first commit

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -14,3 +14,9 @@ function useCurrencyInfo(currency){
 }
 
 export default useCurrencyInfo;
+ git init
+ git add .
+ git commit -m "first commit"
+ git branch -M main
+ git remote add origin https://github.com/peeyush50/Currency_converter.git
+  git push -u origin main
